385319c61c6a37bb802cd618a7852c228b910eb7
ahmfer <3tomu24.feroah@folkuniversitetet.nu>
2026-01-26 21:32:49 +0100

    Add tests for ButtonBean and ButtonModel

---
+++ src/main/java/com/mycompany/demo/ButtonModelTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class ButtonModelTest {
+    @Test
+    void testGetLabel() {
+
+    }
+
+    @Test
+    void testIsDisabled() {
+
+    }
+}
