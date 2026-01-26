2db978fde1f2ae1fbdcf63082ad1718684f547f1
AdrianK2001 <3tomu24.kaczad@folkuniversitetet.nu>
2026-01-26 15:25:31 +0100

    Added java classes for Button

---
+++ src/main/java/com/mycompany/demo/ButtonModel.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.demo;
+
+public class ButtonModel {
+    
+    private String label;
+    private boolean disabled;
+    
+    public ButtonModel(String label, boolean disabled) {
+        this.label = label;
+        this.disabled = disabled;
+    }
+    
+    public String getLabel() {
+        return label;
+    }
+    
+    public boolean isDisabled(){
+        return disabled;
+    }
+}
