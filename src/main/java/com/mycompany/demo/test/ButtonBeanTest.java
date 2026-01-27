ad9e3870e24afb19ea54f40230e8a96f431a08e6
ahmfer <3tomu24.feroah@folkuniversitetet.nu>
2026-01-27 11:02:37 +0100

    Add test files for ButtonBean and ButtonModel

---
+++ src/main/java/com/mycompany/demo/test/ButtonBeanTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.demo.test;
+
+import org.junit.jupiter.api.Test;
+
+public class ButtonBeanTest {
+    @Test
+    void testGetButtonModel() {
+
+    }
+
+    @Test
+    void testOnClick() {
+
+    }
+}
---
+++ src/main/java/com/mycompany/demo/test/ButtonModelTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.demo.test;
+
+import org.junit.jupiter.api.Test;
+
+public class ButtonModelTest {
+    @Test
+    void testGetLabel() {
+
+    }
+
+    @Test
+    void testIsDisabled() {
+
+    }
+}
