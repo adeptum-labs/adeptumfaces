828fbfa5a1c457b43c370ef5485b4d42da904438
ahmfer <3tomu24.feroah@folkuniversitetet.nu>
2026-01-27 15:49:15 +0100

    Add JUnit tests for ButtonBean and ButtonModel

--- src/main/java/com/mycompany/demo/test/ButtonBeanTest.java
+++ src/main/java/com/mycompany/demo/test/ButtonBeanTest.java
@@ -1,6 +1,11 @@
 package com.mycompany.demo.test;
 
+import com.mycompany.demo.ButtonBean;
+import com.mycompany.demo.ButtonModel;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class ButtonBeanTest {
     @Test
--- src/main/java/com/mycompany/demo/test/ButtonModelTest.java
+++ src/main/java/com/mycompany/demo/test/ButtonModelTest.java
@@ -1,6 +1,10 @@
 package com.mycompany.demo.test;
 
-import org.junit.jupiter.api.Test;
+import com.mycompany.demo.ButtonBean;
+import com.mycompany.demo.ButtonModel;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ButtonModelTest {
     @Test
